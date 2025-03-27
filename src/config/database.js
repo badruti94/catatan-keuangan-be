9295470faa07930376891f8a9cc886b1f132655d
Badru <badruakfm@gmail.com>
2025-03-28 02:13:12 +0700

    update vercel json

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
-    // logging: false,
+    logging: false,
 });
 
 module.exports = sequelize;
