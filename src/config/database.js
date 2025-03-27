3d2ae407dbe6607149aa04a4c30ad8612b13f76f
Badru <badruakfm@gmail.com>
2025-03-28 02:21:44 +0700

    update pg

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+const pg = require('pg');
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
+    dialectModule: pg,
     logging: false,
 });
 
